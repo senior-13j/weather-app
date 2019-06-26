f199078af489481da5f9afe7976c9c73eebcc2d2
ArkadiyJ <mr.kotlyaroff@gmail.com>
2019-06-26 20:01:01 +0400

    Updates

---
+++ precache-manifest.f651873b3deb84cb3fc2a4159a0d46bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93ead1aa18b14a9c660cbf608d269e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565f5c3619efa00a24e7",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "565f5c3619efa00a24e7",
+    "url": "/static/js/main.dafa82bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
