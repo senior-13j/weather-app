350a5ee732eccf36fb00007dde7d48f98d1b07cc
ArkadiJ <mr.kotlyaroff@gmail.com>
2019-06-28 15:15:25 +0300

    Добавил содержимое отдельных компонентов (пока что просто для того, чтобы было красиво, потом уже буду генерировать содержимое и передавать отдельно каждому компоненту)

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './../App.css';
-
+import sun from './sun.png';
 const Item = () => {
   return (
     <div className="Item">
-      
+      <div className="content">
+        <p className="text1">Сегодня</p>
+        <h2 className="text2">2 июня</h2>
+        <img src={sun} alt="sun img"/>
+        <p className="text3">днем +26<sup>°</sup></p>
+        <p className="text4">ночью +18<sup>°</sup></p>
+        <p className="text5">ясно,<br/>без осадков</p>
+      </div>
     </div>
   );
 }
