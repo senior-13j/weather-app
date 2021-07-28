d4df0daa630e3be2b5ca6cd5cdcccbb583f7b78f
Arkadiy Kotlyarov <senior_13j@MBP-Arkadiy.netis>
2021-07-29 01:29:30 +0400

    removed unused function

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./Title.module.css";
 import { getMonthDay } from "./../../functions/dates.js";
-import { getWeekDay } from "./../../functions/dates.js";
 export const Title = () => (
   <div className={styles.Title}>
     <h1>Weather Forecast</h1>
